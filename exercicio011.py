c6c141c555d200a09aea6d51d5c39e48b5bb9985
Larissa Carvalho <carvalholarissa530@gmail.com>
2025-02-05 06:25:38 -0300

    exercício até aula 7

--- exercicio011.py
+++ exercicio011.py
@@ -1,8 +1,9 @@
 # Leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².
 
-largura_parede = int(input('Largura da parede: '))
-altura_parede = int(input('Altura da parede: '))
+largura_parede = float(input('Largura da parede: '))
+altura_parede = float(input('Altura da parede: '))
 area = largura_parede * altura_parede
 qtd_tintas_necessarias = area / 2
 
-print(f'Para pintar uma área de {area}m² será necessário {qtd_tintas_necessarias} latas de tinta. ')
+print(f'Sua parede tem a dimensão de {largura_parede}x{altura_parede} e sua área é de {area}m².')
+print(f'Para pintar essa parede, você precisará de {qtd_tintas_necessarias}l de tinta.')
---
+++ exercicio014.py
@@ -0,0 +1,6 @@
+# Converta uma temperatura digitada em °C para °F
+
+temperatura_celsius = float(input('Informe a temperatura em °C: '))
+temperatura_fahrenheit = temperatura_celsius * 9 / 5 + 32
+
+print(f'A temperatura de {temperatura_celsius}°C corresponde a {temperatura_fahrenheit}°F!')
