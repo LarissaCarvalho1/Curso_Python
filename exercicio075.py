92b6f021047d8b0838c53037c5cb806b8d93046b
Larissa Carvalho <carvalholarissa530@gmail.com>
2025-03-07 11:32:17 -0300

    exercicios aula 16

---
+++ exercicio075.py
@@ -0,0 +1,7 @@
+'''
+Leia 4 valores pelo teclado e guarde-os em uma tupla. No final, mostre: 
+
+- Quantas vezes apareceu o valor 9.
+- Em que posição foi digitado o primeiro valor 3.
+- Quais foram os números pares.
+'''
